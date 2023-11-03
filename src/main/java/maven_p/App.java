9061e35f22e150242581aa21777be8df0918a2a2
KiranNataraj <46237860+KiranNataraj@users.noreply.github.com>
2023-11-03 14:35:29 +0530

    Update App.java

--- src/main/java/maven_p/App.java
+++ src/main/java/maven_p/App.java
@@ -3,6 +3,6 @@ package maven_p;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        System.out.println("Hello World JAVA !");
     }
 }
